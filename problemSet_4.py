eaa3bd8f30263acdecb57719f16df0f01d3155e1
BorisKlco <122160506+BorisKlco@users.noreply.github.com>
2023-01-29 20:47:24 +0100

    working on Libraries

---
+++ problemSet_4.py
@@ -0,0 +1,87 @@
+import inquirer
+import sys
+import emoji
+from pyfiglet import Figlet
+import random
+
+"""
+Source: https://cs50.harvard.edu/python/2022/psets/3
+1. Emojize
+2. Frank,Ian and Glens Letters 
+3. Adieu
+4. Guessing
+5. Professor
+6. Bitcoin
+"""
+
+try:
+
+    # 1. Emojize
+    def emojize():
+        userInput = input("emoji text: ")
+        print(emoji.emojize(userInput))
+        pickProblem()
+
+    # 2. Frank,Ian and Glens Letters
+    def frank():
+        userInput = input("Input: ")
+        figlet = Figlet()
+        font = figlet.getFonts()
+        if len(sys.argv) < 2:
+            randomFont = figlet.setFont(font=font[random.randint(1, 425)])
+            print(figlet.renderText(userInput))
+        elif sys.argv[1] == "--font" or sys.argv[1] == "-f":
+            try:
+                figlet.setFont(font=sys.argv[2])
+            except:
+                randomFont = figlet.setFont(font=font[random.randint(1, 425)])
+            print(figlet.renderText(userInput))
+        else:
+            print("Something WoOoOng?!")
+
+    # problemSet_4.py
+    def pickProblem():
+        problemSet = [
+            inquirer.List(
+                "problem",
+                message="What problem set you wanna run?",
+                choices=[
+                    "1. Emojize",
+                    "2. Frank",
+                    "3. Adieu",
+                    "4. Guessing",
+                    "5. Professor",
+                    "6. Bitcoin",
+                    "None, just quit()",
+                ],
+            ),
+        ]
+
+        match inquirer.prompt(problemSet):
+            case {"problem": "1. Emojize"}:
+                print("Emojize:")
+                emojize()
+
+            case {"problem": "2. Frank"}:
+                print("Frank:")
+                frank()
+
+            case {"problem": "3. Adieu"}:
+                print("Adieu:")
+
+            case {"problem": "4. Guessing"}:
+                print("Guessing:")
+
+            case {"problem": "5. Professor"}:
+                print("Professor:")
+
+            case {"problem": "6. Bitcoin"}:
+                print("Bitcoin:")
+
+            case {"problem": "None, just quit()"}:
+                sys.exit("Bye!")
+
+    pickProblem()
+
+except KeyboardInterrupt:
+    sys.exit("\nBye!")
