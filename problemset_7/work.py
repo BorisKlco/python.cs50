f44d946574f30b849fac815306b9dcbb3f2c8d69
Boris Klco <122160506+BorisKlco@users.noreply.github.com>
2023-02-07 13:06:10 +0000

    Dolly👀

--- problemset_7/work.py
+++ problemset_7/work.py
@@ -1,13 +1,45 @@
+import sys
+
+
 def work():
-    return time(input("Time: "))
+  return time(input("Time: "))
 
 
 def time(input):
+  try:
+    time = []
     h1, h2 = input.split(" to ")
-    m1 = h1.split(":")
-    m2 = h2.split(":")
-    print(m1[0].replace(" AM", ""))
-    print(m1[1].replace(" AM", ""))
+    h1 = h1.replace(" AM", "")
+    h2 = h2.replace(" PM", "")
+
+    if ':' in h1:
+      h1, m1 = h1.split(":")
+      if int(h1) < 12 and int(m1) < 60:
+        time.append(f"{h1}:{m1}")
+      else:
+        sys.exit('bad format AM HM')
+    else:
+      if int(h1) < 12:
+        time.append(f"{h1}:00")
+      else:
+        sys.exit('bad format AM H')
+
+    if ':' in h2:
+      h2, m2 = h2.split(":")
+      if int(h2) < 12 and int(m2) < 60:
+        time.append(f"{int(h2)+12}:{m2}")
+      else:
+        sys.exit('bad format PM HM')
+    else:
+      if int(h2) < 12:
+        time.append(f"{int(h2)+12}:00")
+      else:
+        sys.exit('bad format PM H')
+
+    print(time[0], 'to', time[1])
+
+  except:
+    sys.exit('bad format')
 
 
 work()
