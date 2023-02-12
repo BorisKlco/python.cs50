f2faab89079768e1698d97536dd5955f8d97ab46
Boris Klco <122160506+BorisKlco@users.noreply.github.com>
2023-02-12 10:53:21 +0000

    date diff to words

--- problemSet_8/love.py
+++ problemSet_8/love.py
@@ -1,6 +1,21 @@
+import inflect
+import datetime 
 from datetime import date
 
+p = inflect.engine()
+time = datetime.datetime.now()
+
 def main():
+    birth = input('Date:')
+    birthY, birthM, birthD = birth.split('-')
+    print(birthY, birthM, birthD)
+    todayY = time.year
+    todayM = time.month
+    todayD = time.day
+
+    diff = date(todayY, todayM, todayD) - date(int(birthY), int(birthM), int(todayD))
+    print(p.number_to_words(diff.days))
+
 
 
 if __name__ == "__main__":
